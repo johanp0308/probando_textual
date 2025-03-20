11b3cce05449d3d2a39921c692854d533fc8a91a
johanp0308 <111256873+johanp0308@users.noreply.github.com>
2025-03-20 14:47:18 -0500

    feat: :tada: Init commit

---
+++ test_textual.py
@@ -0,0 +1,11 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer, Static
+
+class MiApp(App):
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Static("¡Hola desde Textual!", id="mensaje")
+        yield Footer()
+
+if __name__ == "__main__":
+    MiApp().run()
